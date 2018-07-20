c2ea97161a79019a0684882a60418b5020fc53f4
CWINET\lucas.beck <lucas.beck@cwi.com.br>
2018-07-20 16:35:19 -0300

    Teste de email com erro de build

--- TesteGitHub/ConversorTemperatura.cs
+++ TesteGitHub/ConversorTemperatura.cs
@@ -8,7 +8,18 @@ namespace TesteGitHub
 {
     public class ConversorTemperatura
     {
-        int varial = 0;
-        int variavel2 = 1 / 1;
+        public int Campo1 { get; set; }
+        public int Campo2 { get; set; }
+
+        public ConversorTemperatura()
+        {
+            this.Campo1 = ;
+            this.Campo2 = 1;
+        }
+
+        public int Soma()
+        {
+            return this.Campo1 + this.Campo2;
+        }
     }
 }
