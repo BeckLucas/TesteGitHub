fb38db1725f742398cc255aa8cf527c8950cdd25
Lucas Beck <lucas.beck@cwi.com.br>
2018-07-27 17:21:40 -0300

    Revert "Teste exclusão commit"
    This reverts commit 5e26c2cc83dd0aa05dadfd7e37a436a81bee1d5e.

--- TesteGitHubWF/Program.cs
+++ TesteGitHubWF/Program.cs
@@ -17,7 +17,6 @@ namespace TesteGitHubWF
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
         }
     }
 }
