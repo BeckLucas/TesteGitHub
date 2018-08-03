4a41243c83f0db321dcd19437deb9b7c78231cd2
CWINET\felipe.junges <felipe.junges@cwi.com.br>
2018-08-03 14:43:45 -0300

    Funções novas importantes

--- TesteGitHub/ConversorDatas.cs
+++ TesteGitHub/ConversorDatas.cs
@@ -8,6 +8,16 @@ namespace TesteGitHub
 {
     class ConversorDatas
     {
+		public int Soma(int a, int b)
+		{
+			return a + b;
+		}
+		
+		public int Soma()
+		{
+			return 30;
+		}
+		
         public bool isFunction()
         {
             return true;
